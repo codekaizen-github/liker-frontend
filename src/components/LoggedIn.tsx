c6a4aa2477c4bf90152cd000fee143b80de527ac
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-09-02 11:50:48 +0000

    Automatically poll for new events when first started

--- src/components/LoggedIn.tsx
+++ src/components/LoggedIn.tsx
@@ -4,16 +4,17 @@ import useWebSocket from 'react-use-websocket';
 interface LoggedInProps {
     email: string;
 }
-import { getFetchUpstream } from '../fetchUpstream';
-import onEvent from '../onEvent';
+import { getFetchUpstream as buildFetchUpstreamFunction } from '../fetchUpstream';
+import onEvent, { fetchMissing } from '../onEvent';
 export function LoggedIn({ email }: LoggedInProps) {
     const WS_URL = 'ws://localhost:3030';
     const HTTP_URL_PARSED = new URL('http://localhost:3028/userEvent');
     HTTP_URL_PARSED.searchParams.append('email', email);
-    const fetchUpstream = getFetchUpstream(HTTP_URL_PARSED.toString());
+    const fetchUpstream = buildFetchUpstreamFunction(HTTP_URL_PARSED.toString());
     const { lastJsonMessage } = useWebSocket(`${WS_URL}?email=${email}`, {
-        onOpen: () => {
+        onOpen: async () => {
             console.log('WebSocket connection established.');
+            await fetchMissing(fetchUpstream);
         },
         onMessage: async (message) => {
             await onEvent(JSON.parse(message.data), fetchUpstream);
--- src/onEvent.ts
+++ src/onEvent.ts
@@ -21,41 +21,7 @@ export default async function onEvent(
     console.log({ event });
     // Get the upstream control lock
     try {
-        const results = await db.transaction(
-            'rw',
-            [db.upstreamControl, db.streamOut],
-            async (trx) => {
-                console.log('inside transaction');
-                // const upstreamForUpdateLock =
-                //     await getUpstreamControlForUpdate(trx, 0); // Prevents duplicate entry keys and insertions in other tables
-                const upstreamControlIgnore =
-                    await insertIntoIgnoreUpstreamControl(trx, {
-                        id: 0,
-                        streamOutId: 0,
-                    });
-                console.log({ upstreamControlIgnore });
-                const upstreamControl = await getUpstreamControlForUpdate(
-                    trx,
-                    0
-                );
-                console.log({ event, upstreamControl });
-                if (upstreamControl === undefined) {
-                    throw new Error('Failed to get upstream control lock');
-                }
-                if (upstreamControl.streamOutId >= event.userEventId) {
-                    throw new StreamEventIdDuplicateException();
-                }
-                if (upstreamControl.streamOutId + 1 === event.userEventId) {
-                    console.log('we have a winner!');
-                    const results = await processStreamEvent(trx, event);
-                    await updateUpstreamControl(trx, 0, {
-                        streamOutId: event.userEventId,
-                    });
-                    return results;
-                }
-                throw new StreamEventOutOfSequenceException();
-            }
-        );
+        const results = await onEventProcessSingle(event);
         if (results.length) {
             for (const result of results) {
                 notifySubscribers(result);
@@ -68,102 +34,99 @@ export default async function onEvent(
         }
         if (e instanceof StreamEventOutOfSequenceException) {
             console.log('Out of sequence event ID', event);
-            const upstreamControl = await db.transaction(
-                'rw',
-                [db.upstreamControl],
-                async (trx) => {
-                    // const upstreamForUpdateLock =
-                    //     await getUpstreamControlForUpdate(trx, 0); // Prevents duplicate entry keys and insertions in other tables
-                    const upstreamControlIgnore =
-                        await insertIntoIgnoreUpstreamControl(trx, {
-                            id: 0,
-                            streamOutId: 0,
-                        });
-                    const upstreamControl = await getUpstreamControlForUpdate(
-                        trx,
-                        0
-                    );
-                    return upstreamControl;
+            await fetchMissing(fetchUpstream);
+        }
+    }
+}
+
+export async function onEventProcessSingle(event: any) {
+    const results = await db.transaction(
+        'rw',
+        [db.upstreamControl, db.streamOut],
+        async (trx) => {
+            const upstreamForUpdateLock = await getUpstreamControlForUpdate(
+                trx,
+                0
+            ); // Prevents duplicate entry keys and insertions in other tables
+            const upstreamControlIgnore = await insertIntoIgnoreUpstreamControl(
+                trx,
+                {
+                    id: 0,
+                    streamOutId: 0,
                 }
             );
+            const upstreamControl = await getUpstreamControlForUpdate(trx, 0);
+
+            console.log({
+                event: event,
+                upstreamControl,
+            });
             if (upstreamControl === undefined) {
                 throw new Error('Failed to get upstream control lock');
             }
-            const streamOuts = await fetchUpstream(upstreamControl.streamOutId);
-            console.log({ streamOuts });
-            for (const streamOut of streamOuts) {
-                console.log('...next interation!');
-                try {
-                    const results = await db.transaction(
-                        'rw',
-                        [db.upstreamControl, db.streamOut],
-                        async (trx) => {
-                            const upstreamForUpdateLock =
-                                await getUpstreamControlForUpdate(trx, 0); // Prevents duplicate entry keys and insertions in other tables
-                            const upstreamControlIgnore =
-                                await insertIntoIgnoreUpstreamControl(trx, {
-                                    id: 0,
-                                    streamOutId: 0,
-                                });
-                            const upstreamControl =
-                                await getUpstreamControlForUpdate(trx, 0);
+            if (upstreamControl.streamOutId >= event.userEventId) {
+                throw new StreamEventIdDuplicateException();
+            }
+            if (upstreamControl.streamOutId + 1 === event.userEventId) {
+                console.log('we have a winner! on 2nd pass');
+                const results = await processStreamEvent(trx, event);
+                await updateUpstreamControl(trx, 0, {
+                    id: 0,
+                    streamOutId: event.userEventId,
+                });
+                return results;
+            }
+            throw new StreamEventOutOfSequenceException();
+        }
+    );
+    return results;
+}
 
-                            console.log({
-                                streamOut,
-                                upstreamControl,
-                            });
-                            if (upstreamControl === undefined) {
-                                throw new Error(
-                                    'Failed to get upstream control lock'
-                                );
-                            }
-                            if (
-                                upstreamControl.streamOutId >=
-                                streamOut.userEventId
-                            ) {
-                                throw new StreamEventIdDuplicateException();
-                            }
-                            if (
-                                upstreamControl.streamOutId + 1 ===
-                                streamOut.userEventId
-                            ) {
-                                console.log('we have a winner! on 2nd pass');
-                                const results = await processStreamEvent(
-                                    trx,
-                                    streamOut
-                                );
-                                await updateUpstreamControl(trx, 0, {
-                                    id: 0,
-                                    streamOutId: streamOut.userEventId,
-                                });
-                                return results;
-                            }
-                            throw new StreamEventOutOfSequenceException();
-                        }
-                    );
-                    if (results.length) {
-                        for (const result of results) {
-                            notifySubscribers(result);
-                        }
-                    }
-                } catch (e) {
-                    if (e instanceof StreamEventIdDuplicateException) {
-                        console.log(
-                            'Duplicate event ID on 2nd pass',
-                            streamOut
-                        );
-                        continue;
-                    }
-                    if (e instanceof StreamEventOutOfSequenceException) {
-                        console.log('Out of sequence event ID on 2nd pass', {
-                            upstreamControl,
-                            streamOut,
-                        });
-                        continue;
-                    }
-                    throw e;
+export async function fetchMissing(fetchUpstream: FetchUpstream) {
+    const upstreamControl = await db.transaction(
+        'rw',
+        [db.upstreamControl],
+        async (trx) => {
+            // const upstreamForUpdateLock =
+            //     await getUpstreamControlForUpdate(trx, 0); // Prevents duplicate entry keys and insertions in other tables
+            const upstreamControlIgnore = await insertIntoIgnoreUpstreamControl(
+                trx,
+                {
+                    id: 0,
+                    streamOutId: 0,
+                }
+            );
+            const upstreamControl = await getUpstreamControlForUpdate(trx, 0);
+            return upstreamControl;
+        }
+    );
+    if (upstreamControl === undefined) {
+        throw new Error('Failed to get upstream control lock');
+    }
+    const events = await fetchUpstream(upstreamControl.streamOutId);
+    console.log({ events: events });
+    for (const event of events) {
+        console.log('...next interation!');
+        try {
+            const results = await onEventProcessSingle(event);
+            if (results.length) {
+                for (const result of results) {
+                    notifySubscribers(result);
                 }
             }
+        } catch (e) {
+            if (e instanceof StreamEventIdDuplicateException) {
+                console.log('Duplicate event ID on 2nd pass', event);
+                continue;
+            }
+            if (e instanceof StreamEventOutOfSequenceException) {
+                console.log('Out of sequence event ID on 2nd pass', {
+                    upstreamControl,
+                    event: event,
+                });
+                continue;
+            }
+            throw e;
         }
     }
 }
