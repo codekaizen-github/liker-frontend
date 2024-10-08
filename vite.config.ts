ee943f966b875bea7e146afc152081ae9a2359fb
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-10-08 21:32:43 +0000

    Set up local dev to run behind a reverse proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), basicSsl()],
+    plugins: [react()],
     // https://github.com/vitejs/vite/discussions/14739
     server: {
         port: 80,
